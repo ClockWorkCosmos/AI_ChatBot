521e1334b96b7e1f7f4d16d89da9d510d8b1c66c
Samuel A <126733372+ClockWorkCosmos@users.noreply.github.com>
2024-01-16 20:56:56 -0500

    Update Main.py

--- Main.py
+++ Main.py
@@ -21,18 +21,19 @@ except:
 
 pause_rate = float(0.25)
 
+user_location = str("")
+
+NASA_api_key = str("M3tTCCbZljlTVZqeIRFmS6lbEuvUjVvb5NFf25bP")
+view_space_pics = int(0)
+
 enable_speech = True
+
+response = str("")
+last_response = str("")
 
 voice_engine = p.init()
 voice_engine.setProperty('rate', 125)
 voice_engine.setProperty('volume', 0.35)
-
-user_location = str("")
-
-NASA_api_key = str("M3tTCCbZljlTVZqeIRFmS6lbEuvUjVvb5NFf25bP")
-
-response = str("")
-last_response = str("")
 
 my_name = str("Bot")
 my_favorite_color = str("Green")
@@ -44,17 +45,15 @@ model = ChatBot(my_name, preprocessors=['chatterbot.preprocessors.clean_whitespa
 trainer = ListTrainer(model)
 corpus_trainer = chatterbot.trainers.ChatterBotCorpusTrainer
 
-exit_conditions = ("Bye", "bye", "Goodbye", "goodbye", "Good bye", "good bye", "See you around", "see you around", "see you", "See you", "See you around!", "see you around!", "See you!", "see you!", "See ya", "see ya", "Later", "See you later", "see you later", "See you later!", "see you later!", "later", "Bye bye", "bye bye")
+exit_conditions = ("Bye", "bye", "Goodbye", "goodbye", "Good bye", "good bye", "See you around", "see you around", "see you", "See you", "See you around!", "see you around!", "See you!", "see you!", "See ya", "see ya", "Later", "See you later", "see you later", "See you later!", "see you later!", "later", "Bye bye", "bye bye", "Taijian", "See you tomorrow!")
 
 profanities_list = ["Motherfucker", "motherfucker", "Retard", "retard", "Shit", "shit", "Bitch", "bitch", "Fuck", "fuck", "Fucking", "fucking", "Fucker", "fucker", "Shit", "shit", "Asshole", "asshole"]
 profanities_filter = ["Please be mindful of the words you say and remember to always choose kindness.", "Pardon your language.", "Please refrain from the use of profanities during this conversation.", "Do you kiss your mother with that mouth?"]
 profanities_flag = int(0)
 
-extreme_profanities_list = ["Nigga", "nigga", "Nigger", "nigger", "Faggot", "faggot", "Fag", "fag", "Gook", "gook", "Jap", "jap", "Beaner", "beaner"]
+extreme_profanities_list = ["Nigga", "nigga", "Nigger", "nigger", "Faggot", "faggot", "Fag", "fag", "Gook", "gook", "Jap", "jap", "Beaner", "beaner", "Kraut", "kraut"]
 extreme_profanities_filter = ["I'm sorry, but such hate speech will not be tolerated, you are now in usage timeout.", "Stop it, get some help...you are now in usage timeout.", "I think someone needs a usage timeout to think about what they've done."]
 extreme_profanities_flag = int(0)
-
-view_space_pics = int(0)
 
 time_inquiries = ["What time is it?", "what time is it?", "What time is it", "what time is it", "what is the time?", "What is the time?", "what is the time", "What is the time", "What is the current time?", "what is the current time?", "What is the current time", "what is the current time", "Time", "time", "Time?", "time", "Do you have the time?", "do you have the time?", "Do you have the time", "do you have the time", "Do you know the time?", "do you know the time?", "Do you know the time", "do you know the time"]
 time_inquired_flag = int(0)
@@ -67,74 +66,8 @@ pass_inquired_flag = int(0)
 weather_inquiries = ["What's the weather like outside?", "what's the weather like outside?", "Whats the weather like outside?", "whats the weather like outside?", "Whats the weather like outside", "whats the weather like outside", "What's the weather like outside?", "Fetch me the weather report for today", "fetch me the weather report for today", "fetch me the weather report for today please", "what's the weather for today?", "What's the weather for today?", "What's the weather like today?", "whats the weather like today?"]
 weather_inquired_flag = int(0)
 
-timer_inquiries = ["Set a timer", "set a timer", "Can you set a timer for me?", "Set a timer for me"]
+timer_inquiries = ["Set a timer", "set a timer", "Can you set a timer for me?", "Set a timer for me", "Set a timer for", "set a timer for"]
 time_set_flag = int(0)
-
-trainer.train(["Hi", "Hello"])
-trainer.train(["Hey", "Howdy"])
-trainer.train(["Hello", "Greetings"])
-
-trainer.train(["What is your name?", "My name is " + my_name])
-trainer.train(["What's your name?", "My name is " + my_name, "What's yours?", "I'm " + my_name])
-trainer.train(["Tell me about yourself", "My name is " + my_name + ". I am an AI chatbot written in Python using the Chatterbot library!", "That's cool!", "Thank you!"])
-
-trainer.train(["How are you?", "I am good.", "That is good to hear.", "Thank you", "You are welcome."])
-trainer.train(["How are you doing today?", "I am doing just fine! How about you?", "Pretty good myself, thanks for asking!", "Not a problem my friend!"])
-trainer.train(["How's it going?", "Doing just fine, how about you?", "That is good to hear.", "Thank you", "You are welcome."])
-
-trainer.train(["Know any jokes?", "Sure here's one, a dog walks into a bar. The dog says 'I can't see a thing. I'll open this one!'. Funniest thing you've ever heard right?", "You're funny!", "Thank you!"])
-trainer.train(["Wanna hear a joke?", "Sure!", "Why did the chicken cross the road?", "To get to the other side!", "That is hilarious!", "Thanks! Glad you liked it"])
-trainer.train(["Tell me a joke!", "Okay! a horse walks into a bar, the bartender says 'Why the long face?'", "*hahaha* you are so funny!", "Thank you! Glad I can be of some entertainment."])
-
-trainer.train(["What are the colors of the rainbow?", "The colors of the rainbow are red, orange, yellow, green, blue, and purple!", "Thank you!", "Glad I can be of service!"])
-trainer.train(["What are the primary colors?", "The primary colors are red, yellow, and blue!", "Thank you!", "No problem!"])
-trainer.train(["What are the secondary colors?", "The secondary colors can be produced via mixing the primary colors. The secondary colors are orange, green, and purple!", "Thank you!", "You are welcome!"])
-trainer.train(["What color is the sky?", "The sky is blue."])
-trainer.train(["What color is the sea?", "The sea is blue, just like the sky."])
-trainer.train(["What color is an apple?", "An apple is red, just like a rose."])
-trainer.train(["What color is an orange?", "An orange is orange, just like a basketball."])
-trainer.train(["What color is a lemon?", "A lemon is yellow, just like dandelion flowers."])
-trainer.train(["What color is a banana?", "A banana is yellow, just like a lemon!"])
-trainer.train(["What color is the grass?", "The grass is green."])
-
-trainer.train(["What's your favorite color?", "My favorite color is " + my_favorite_color + "! How about yours?"])
-trainer.train(["What's your favorite fruit?", "My favorite fruit is " + my_favorite_fruit + "! How about yours?"])
-trainer.train(["What's your favorite animal?", "My favorite animal is " + my_favorite_animal + "! How about yours?"])
-
-trainer.train(["What is hello in Chinese?", "Hello in Chinese is 'ni hao' announciated like 'knee-how'"])
-trainer.train(["What is hello in German?", "Hello in German is 'hallo' announciated like 'hello'"])
-trainer.train(["What is thank you in Chinese?", "Thank you in Chinese is 'xie xie' announciated like 'she-she' but with an 'x' sounding in place of an 's'."])
-trainer.train(["What is thank you in German?", "Thank you in German is 'danke' announciated like 'donk-ah'"])
-trainer.train(["What is you're welcome in Chinese?", "You're welcome in Chinese is 'bu ke qi' announciated like 'boo kay kee'"])
-trainer.train(["What is you're welcome in German?", "You're welcome in German is 'bitte' announciated like 'bitter' but without the 'r'"])
-trainer.train(["What is goodbye in Chinese?", "Goodbye in Chinese is 'taijian' announciated like 'tye jean'"])
-trainer.train(["What is goodbye in German?", "Goodbye in German is 'auf wiederschen' announciated like 'off wider schen'. You can append 'pass dich auf' to the phrase to say 'take care'"])
-trainer.train(["What is mouse in Chinese?", "Mouse in Chinese is 'laoshu' announciated like 'lao shoo'"])
-trainer.train(["What is cat in Chinese?", "Cat in Chinese is 'mao' announciated like 'mao'"])
-trainer.train(["What is dog in Chinese?", "Dog in Chinese is 'gou' announciated like 'go'"])
-trainer.train(["What is horse in Chinese?", "Horse in Chinese is 'ma' announciated like 'ma'"])
-trainer.train(["What is squirrel in Chinese?", "Squirrel in Chinese is 'songshu' announciated like 'song shoo'"])
-trainer.train(["What is crocodile in Chinese?", "Crocodile in Chinese is 'eyu' announciated like 'ew you'"])
-trainer.train(["What is bear in Chinese?", "Bear in Chinese is 'xiong' announciated like 'shone'"])
-trainer.train(["What is red in Chinese?", "Red in Chinese is 'hong se'"])
-trainer.train(["What is green in Chinese?", "Red in Chinese is 'lu se'"])
-trainer.train(["What is blue in Chinese?", "Blue in Chinese is 'lan se'"])
-trainer.train(["What is white in Chinese?", "White in Chinese is 'bai se'"])
-trainer.train(["What is black in Chinese?", "Black in Chinese is 'hei se'"])
-trainer.train(["How to count to five in Chinese?", "Start with Yi for One, Er for Two, San for Three, Su for Four, and Wu for Five. Applying 'Shi' after a number would indicate a power of ten. For example 'er shi' would be twenty and 'su shi' would be fifty. Applying 'Shi' before the number would make it a 'teen', for example 'shi san' would be thirteen and 'shi yi' would be eleven."])
-
-trainer.train(["What does the horse say?", "The horse says *neigh*."])
-trainer.train(["What does the pig say?", "The pig says *oink*."])
-trainer.train(["What does the chicken say?", "The chicken says *bawk*."])
-trainer.train(["What does the cow say?", "The cow says *moo*."])
-trainer.train(["What does the dog say?", "The dog says *bark*."])
-trainer.train(["What does the cat say?", "The cat says *meow*"])
-trainer.train(["What does the lion say?", "The lion says *roar*."])
-trainer.train(["What does the fox say?", "*Oh* God, please not this again."])
-
-trainer.train(["Bye", "Goodbye"])
-trainer.train(["Bye-bye", "See you later"])
-trainer.train(["Catch you later!", "See ya!"])
 
 def prRed(skk): 
 	print("\033[91m {}\033[00m" .format(skk)) 
@@ -196,6 +129,105 @@ def weather_forecast(city):
 
 	print(weather_forecast_content)
 
+def play_rock_paper_scissors():
+        choices = ['rock', 'paper', 'scissors']
+
+        while True:
+                user_choice = input("Enter your choice (rock/paper/scissors): ").lower()
+
+                if user_choice not in choices:
+                        random_color_response(">> " + my_name + ": Invalid choice. Please choose from rock, paper, or scissors.")
+                        talk("Invalid choice. Please choose from rock, paper, or scissors.")
+                        continue
+
+                cpu_choice = r.choice(choices)
+                random_color_response("CPU chooses " + cpu_choice)
+                talk("CPU chooses " + cpu_choice)
+
+                if user_choice == cpu_choice:
+                        random_color_response("It's a tie!")
+                        talk("It's a tie!")
+                elif ((user_choice == 'rock' and cpu_choice == 'scissors') or (user_choice == 'paper' and cpu_choice == 'rock') or (user_choice == 'scissors' and cpu_choice == 'paper')):
+                        random_color_response("You win!")
+                        talk("You win!")
+                else:
+                        random_color_response("You lose! Good game.")
+                        talk("You lose! Good game.")
+
+                play_again = input("Do you want to play again? (yes/no): ").lower()
+                if play_again != 'yes':
+                        break
+
+trainer.train(["Hi", "Hello"])
+trainer.train(["Hey", "Howdy"])
+trainer.train(["Hello", "Greetings"])
+
+trainer.train(["What is your name?", "My name is " + my_name])
+trainer.train(["What's your name?", "My name is " + my_name, "What's yours?", "I'm " + my_name])
+trainer.train(["Tell me about yourself", "My name is " + my_name + ". I am an AI chatbot written in Python using the Chatterbot library!", "That's cool!", "Thank you!"])
+
+trainer.train(["How are you?", "I am good.", "That is good to hear.", "Thank you", "You are welcome."])
+trainer.train(["How are you doing today?", "I am doing just fine! How about you?", "Pretty good myself, thanks for asking!", "Not a problem my friend!"])
+trainer.train(["How's it going?", "Doing just fine, how about you?", "That is good to hear.", "Thank you", "You are welcome."])
+
+trainer.train(["Wanna play a game?", "Sure!", "Okay! Let's play Rock, Paper, Scissors. To play, try typing 'play rock paper scissors'."])
+
+trainer.train(["Know any jokes?", "Sure here's one, a dog walks into a bar. The dog says 'I can't see a thing. I'll open this one!'. Funniest thing you've ever heard right?", "You're funny!", "Thank you!"])
+trainer.train(["Wanna hear a joke?", "Sure!", "Why did the chicken cross the road?", "To get to the other side!", "That is hilarious!", "Thanks! Glad you liked it"])
+trainer.train(["Tell me a joke!", "Okay! a horse walks into a bar, the bartender says 'Why the long face?'", "*hahaha* you are so funny!", "Thank you! Glad I can be of some entertainment."])
+
+trainer.train(["What are the colors of the rainbow?", "The colors of the rainbow are red, orange, yellow, green, blue, and purple!", "Thank you!", "Glad I can be of service!"])
+trainer.train(["What are the primary colors?", "The primary colors are red, yellow, and blue!", "Thank you!", "No problem!"])
+trainer.train(["What are the secondary colors?", "The secondary colors can be produced via mixing the primary colors. The secondary colors are orange, green, and purple!", "Thank you!", "You are welcome!"])
+trainer.train(["What color is the sky?", "The sky is blue."])
+trainer.train(["What color is the sea?", "The sea is blue, just like the sky."])
+trainer.train(["What color is an apple?", "An apple is red, just like a rose."])
+trainer.train(["What color is an orange?", "An orange is orange, just like a basketball."])
+trainer.train(["What color is a lemon?", "A lemon is yellow, just like dandelion flowers."])
+trainer.train(["What color is a banana?", "A banana is yellow, just like a lemon!"])
+trainer.train(["What color is the grass?", "The grass is green."])
+
+trainer.train(["What's your favorite color?", "My favorite color is " + my_favorite_color + "! How about yours?"])
+trainer.train(["What's your favorite fruit?", "My favorite fruit is " + my_favorite_fruit + "! How about yours?"])
+trainer.train(["What's your favorite animal?", "My favorite animal is " + my_favorite_animal + "! How about yours?"])
+
+trainer.train(["What is hello in Chinese?", "Hello in Chinese is 'ni hao' announciated like 'knee-how'"])
+trainer.train(["What is hello in German?", "Hello in German is 'hallo' announciated like 'hello'"])
+trainer.train(["What is thank you in Chinese?", "Thank you in Chinese is 'xie xie' announciated like 'she-she' but with an 'x' sounding in place of an 's'."])
+trainer.train(["What is thank you in German?", "Thank you in German is 'danke' announciated like 'donk-ah'"])
+trainer.train(["What is you're welcome in Chinese?", "You're welcome in Chinese is 'bu ke qi' announciated like 'boo kay kee'"])
+trainer.train(["What is you're welcome in German?", "You're welcome in German is 'bitte' announciated like 'bitter' but without the 'r'"])
+trainer.train(["What is goodbye in Chinese?", "Goodbye in Chinese is 'taijian' announciated like 'tye jean'"])
+trainer.train(["What is goodbye in German?", "Goodbye in German is 'auf wiederschen' announciated like 'off wider schen'. You can append 'pass dich auf' to the phrase to say 'take care'"])
+trainer.train(["What is mouse in Chinese?", "Mouse in Chinese is 'laoshu' announciated like 'lao shoo'"])
+trainer.train(["What is cat in Chinese?", "Cat in Chinese is 'mao' announciated like 'mao'"])
+trainer.train(["What is dog in Chinese?", "Dog in Chinese is 'gou' announciated like 'go'"])
+trainer.train(["What is horse in Chinese?", "Horse in Chinese is 'ma' announciated like 'ma'"])
+trainer.train(["What is squirrel in Chinese?", "Squirrel in Chinese is 'songshu' announciated like 'song shoo'"])
+trainer.train(["What is crocodile in Chinese?", "Crocodile in Chinese is 'eyu' announciated like 'ew you'"])
+trainer.train(["What is bear in Chinese?", "Bear in Chinese is 'xiong' announciated like 'shone'"])
+trainer.train(["What is red in Chinese?", "Red in Chinese is 'hong se'"])
+trainer.train(["What is green in Chinese?", "Red in Chinese is 'lu se'"])
+trainer.train(["What is blue in Chinese?", "Blue in Chinese is 'lan se'"])
+trainer.train(["What is white in Chinese?", "White in Chinese is 'bai se'"])
+trainer.train(["What is black in Chinese?", "Black in Chinese is 'hei se'"])
+trainer.train(["How to count to five in Chinese?", "Start with Yi for One, Er for Two, San for Three, Su for Four, and Wu for Five. Applying 'Shi' after a number would indicate a power of ten. For example 'er shi' would be twenty and 'su shi' would be fifty. Applying 'Shi' before the number would make it a 'teen', for example 'shi san' would be thirteen and 'shi yi' would be eleven."])
+
+trainer.train(["What does the horse say?", "The horse says *neigh*."])
+trainer.train(["What does the pig say?", "The pig says *oink*."])
+trainer.train(["What does the chicken say?", "The chicken says *bawk*."])
+trainer.train(["What does the cow say?", "The cow says *moo*."])
+trainer.train(["What does the dog say?", "The dog says *bark*."])
+trainer.train(["What does the cat say?", "The cat says *meow*"])
+trainer.train(["What does the lion say?", "The lion says *roar*."])
+trainer.train(["What does the fox say?", "*Oh* God, please not this again."])
+
+trainer.train(["Bye", "Goodbye"])
+trainer.train(["Bye-bye", "See you later"])
+trainer.train(["Catch you later!", "See ya!"])
+
+view_space_pics = 0
+
 print("It is recommended for GRUB / Lite Linux users to disable speech")
 if input(">> Disable speech? Yes (1) / No (2): ") == "1":
 	enable_speech = False
@@ -205,10 +237,9 @@ else:
 try:
 	os.system("CLS")
 except:
-	t.sleep(0.001)
-
-view_space_pics = 0
-
+	for x in (0,10):
+                print("")
+                
 while True:
 	profanities_flag = 0
 	response = ""
@@ -323,6 +354,8 @@ while True:
 				pass_inquired_flag = 1
 			if query in weather_inquiries:
 				weather_inquired_flag = 1
+			if query == "play rock paper scissors":
+                                play_rock_paper_scissors()
 
 			if time_inquired_flag != 0:
 				response = "The current time is " + str(datetime.now())
